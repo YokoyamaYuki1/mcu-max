8b54490a5260e7e11248c45c561b874df304b11e
YokoyamaYuki1 <70437809+YokoyamaYuki1@users.noreply.github.com>
2024-12-24 00:37:10 +0900

    Update main.c

--- examples/mcu-max-uci/main.c
+++ examples/mcu-max-uci/main.c
@@ -9,71 +9,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "mcu-max.h"
+#include <time.h>
+#include <stdint.h>
 
 #define MAIN_VALID_MOVES_NUM 512
+#define MOVE_EVALUATION_LIMIT 30 // 上位30手までに制限
 
-void print_board()
+typedef struct
 {
-    const char *symbols = ".PPNKBRQ.ppnkbrq";
+    mcumax_move move;
+    int32_t score;
+} MoveEvaluation;
 
-    printf("  +-----------------+\n");
+int compare_moves(const void *a, const void *b)
+{
+    MoveEvaluation *move_a = (MoveEvaluation *)a;
+    MoveEvaluation *move_b = (MoveEvaluation *)b;
+    return move_b->score - move_a->score;
+}
 
-    for (uint32_t y = 0; y < 8; y++)
+mcumax_move search_with_constraints(uint32_t max_time_ms)
+{
+    mcumax_move best_move = MCUMAX_MOVE_INVALID;
+    clock_t start_time = clock();
+    mcumax_move valid_moves[MAIN_VALID_MOVES_NUM];
+    uint32_t valid_moves_num = mcumax_search_valid_moves(valid_moves, MAIN_VALID_MOVES_NUM);
+
+    MoveEvaluation evaluated_moves[MAIN_VALID_MOVES_NUM];
+    uint32_t evaluated_count = 0;
+
+    for (uint32_t i = 0; i < valid_moves_num; i++)
     {
-        printf("%d | ", 8 - y);
-        for (uint32_t x = 0; x < 8; x++)
-            printf("%c ", symbols[mcumax_get_piece(0x10 * y + x)]);
-        printf("|\n");
+        mcumax_move move = valid_moves[i];
+        mcumax_play_move(move);
+        int32_t score = mcumax_search(-MCUMAX_SCORE_MAX, MCUMAX_SCORE_MAX, 0, MCUMAX_SQUARE_INVALID, 1, MCUMAX_INTERNAL_NODE);
+        mcumax_play_move((mcumax_move){move.to, move.from});
+        evaluated_moves[evaluated_count++] = (MoveEvaluation){move, score};
     }
 
-    printf("  +-----------------+\n");
-    printf("    a b c d e f g h\n");
+    qsort(evaluated_moves, evaluated_count, sizeof(MoveEvaluation), compare_moves);
+    if (evaluated_count > MOVE_EVALUATION_LIMIT)
+        evaluated_count = MOVE_EVALUATION_LIMIT;
 
-    printf("\n");
-}
-
-mcumax_square get_square(char *s)
-{
-    mcumax_square rank = s[0] - 'a';
-    if (rank > 7)
-        return MCUMAX_SQUARE_INVALID;
-
-    mcumax_square file = '8' - s[1];
-    if (file > 7)
-        return MCUMAX_SQUARE_INVALID;
-
-    return 0x10 * file + rank;
-}
-
-bool is_square_valid(char *s)
-{
-    return (get_square(s) != MCUMAX_SQUARE_INVALID);
-}
-
-bool is_move_valid(char *s)
-{
-    return is_square_valid(s) && is_square_valid(s + 2);
-}
-
-void print_square(mcumax_square square)
-{
-    printf("%c%c",
-           'a' + ((square & 0x07) >> 0),
-           '1' + 7 - ((square & 0x70) >> 4));
-}
-
-void print_move(mcumax_move move)
-{
-    if ((move.from == MCUMAX_SQUARE_INVALID) ||
-        (move.to == MCUMAX_SQUARE_INVALID))
-        printf("(none)");
-    else
+    for (uint32_t i = 0; i < evaluated_count; i++)
     {
-        print_square(move.from);
-        print_square(move.to);
+        if ((clock() - start_time) * 1000 / CLOCKS_PER_SEC >= max_time_ms)
+            break;
+
+        mcumax_play_move(evaluated_moves[i].move);
+        int32_t score = mcumax_search(-MCUMAX_SCORE_MAX, MCUMAX_SCORE_MAX, 0, MCUMAX_SQUARE_INVALID, 2, MCUMAX_INTERNAL_NODE);
+        mcumax_play_move((mcumax_move){evaluated_moves[i].move.to, evaluated_moves[i].move.from});
+
+        if (score > best_move.from) // Better move found
+            best_move = evaluated_moves[i].move;
     }
+
+    return best_move;
 }
 
 bool send_uci_command(char *line)
@@ -89,77 +81,26 @@ bool send_uci_command(char *line)
         printf("id author " MCUMAX_AUTHOR "\n");
         printf("uciok\n");
     }
-    else if (!strcmp(token, "uci") ||
-             !strcmp(token, "ucinewgame"))
-        mcumax_init();
-    else if (!strcmp(token, "isready"))
-        printf("readyok\n");
-    else if (!strcmp(token, "d"))
-        print_board();
-    else if (!strcmp(token, "l"))
-    {
-        mcumax_move valid_moves[MAIN_VALID_MOVES_NUM];
-        uint32_t valid_moves_num = mcumax_search_valid_moves(valid_moves, MAIN_VALID_MOVES_NUM);
-
-        for (uint32_t i = 0; i < valid_moves_num; i++)
-        {
-            print_move(valid_moves[i]);
-            printf(" ");
-        }
-        printf("\n");
-    }
-    else if (!strcmp(token, "position"))
-    {
-        int fen_index = 0;
-        char fen_string[256];
-
-        while (token = strtok(NULL, " \n"))
-        {
-            if (fen_index)
-            {
-                strcat(fen_string, token);
-                strcat(fen_string, " ");
-
-                fen_index++;
-                if (fen_index > 6)
-                {
-                    mcumax_set_fen_position(fen_string);
-
-                    fen_index = 0;
-                }
-            }
-            else
-            {
-                if (!strcmp(token, "startpos"))
-                    mcumax_init();
-                else if (!strcmp(token, "fen"))
-                {
-                    fen_index = 1;
-                    strcpy(fen_string, "");
-                }
-                else if (is_move_valid(token))
-                {
-                    mcumax_play_move((mcumax_move){
-                        get_square(token + 0),
-                        get_square(token + 2),
-                    });
-                }
-            }
-        }
-    }
     else if (!strcmp(token, "go"))
     {
-        mcumax_move move = mcumax_search_best_move(1, 30);
-        mcumax_play_move(move);
+        uint32_t movetime_ms = 1000; // デフォルト1秒
+        while ((token = strtok(NULL, " \n")))
+        {
+            if (!strcmp(token, "movetime"))
+            {
+                token = strtok(NULL, " \n");
+                if (token)
+                    movetime_ms = atoi(token);
+            }
+        }
 
+        mcumax_move best_move = search_with_constraints(movetime_ms);
         printf("bestmove ");
-        print_move(move);
+        print_move(best_move);
         printf("\n");
     }
     else if (!strcmp(token, "quit"))
         return true;
-    else
-        printf("Unknown command: %s\n", token);
 
     return false;
 }
@@ -170,12 +111,12 @@ int main()
 
     while (true)
     {
-        fflush(stdout);
-
         char line[65536];
         fgets(line, sizeof(line), stdin);
 
         if (send_uci_command(line))
             break;
     }
+
+    return 0;
 }
